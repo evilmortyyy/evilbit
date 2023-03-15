675211bf1494cc4a474f20ee4e87af344cd376b0
evilmortyyy <88188687+evilmortyyy@users.noreply.github.com>
2023-03-15 06:21:41 -0600

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -194,8 +194,8 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
 const char* const SEED_NODES[] = { 
-  "209.94.58.88:6663",
-  "51.158.161.246:6663",
+  "151.115.59.211:6663",
+  "51.158.237.50:6663",
   "51.159.158.132:6663",
   "51.158.69.253:6663",
   "51.15.82.109:6663"
